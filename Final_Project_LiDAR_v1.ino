3828c7e7c07d84789f3d3882ac264275544fa9ce
nseibold <143371201+nseibold@users.noreply.github.com>
2025-12-11 15:14:32 -0500

    Update Final_Project_LiDAR_v1.ino
    Updated comments and removed debugging

--- Final_Project_LiDAR_v1.ino
+++ Final_Project_LiDAR_v1.ino
@@ -1,92 +1,116 @@
+// Authors: Nate Seibold, Daniel Nalepa
+// Date: 12/12/2025
+// Optoelectronics Final Arduino Code
+
+// import libraries
 #include "Adafruit_VL53L1X.h"
 #include <Stepper.h>
 #include <Wire.h>
- 
+
+// define ToF pins
 #define IRQ_PIN 2
 #define XSHUT_PIN 3
- 
+
+// declare variables
 const int stepsPerRevolution = 2052;
 int16_t distance;
 bool calibrated = false;
-float angleStep = 0;
- 
+
+// initialize ToF and stepper
 Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(XSHUT_PIN, IRQ_PIN);
 Stepper myStepper(stepsPerRevolution, 5, 7, 6, 8);
  
 void setup() {
   Serial.begin(115200);
   delay(100);
-  // Serial.println("LiDAR Project Initialized");
- 
+
+  // warning if ToF can't boot
   if (!vl53.begin()) {
     Serial.println("Failed to initialize VL53L1X!");
     while (1);
   }
-  // begins ranging and timing budget
+ 
+  // sets start-up parameters for Tof and stepper
   bool x;
   x = vl53.startRanging();
   x = vl53.setTimingBudget(20);
   myStepper.setSpeed(4);  
+
+  // run calibration sequence
   calibration();
  
-  // 1 sec delay after calibration
+  // 2 second delay after calibration
   delay(2000);
 }
+
 // taken from Stepper_onRevolution example code
 void loop() {
-  angleStep = 2052.0 / 360.0;
+  // ensure calibration
   if(calibrated) {
+    // cycle through one rotation clockwise, one degree increment
     for (int i = -180; i < 180; i++) {
-      // step one revolution in the other direction:
+      // step one degree (actual is 5.7)
       myStepper.step(-6);
+
+      // hold motor until ToF has refreshed
       while (!vl53.dataReady()) {
-        // do nothing until new data is ready
       }
+
+      // with data ready, gather distance data
       if (vl53.dataReady()) {
-      distance = vl53.distance();
-      Serial.print(i);
-      Serial.print(", ");
-      Serial.println(distance);
-      vl53.clearInterrupt();
-      delay(30);
+        distance = vl53.distance();
+        Serial.print(i); // print angle in degrees
+        Serial.print(", ");
+        Serial.println(distance); // print distance in mm
+        vl53.clearInterrupt(); // clear for next data point
+        delay(30);
+      }
     }
-    }
+   
+    // cycle through one rotation counter-clockwise, one degree increment
     for (int i = 180; i > -180; i--) {
-      // step one revolution  in one direction:
+      // step one degree (actual is 5.7)
       myStepper.step(6);
+
+      // hold motor until ToF has refreshed
       while (!vl53.dataReady()) {
-        // do nothing until new data is ready
       }
+      // with data ready, gather distance data
       if (vl53.dataReady()) {
-      distance = vl53.distance();
-      Serial.print(i);
-      Serial.print(", ");
-      Serial.println(distance);
-      vl53.clearInterrupt();
-      delay(30);
-    }
+        distance = vl53.distance();
+        Serial.print(i); // print angle in degrees
+        Serial.print(", ");
+        Serial.println(distance); // print distance in mm
+        vl53.clearInterrupt(); // clear for next data point
+        delay(30);
+      }
     }
   }
 }
- 
+
+// define calibration function
+// sensor needs to know 180 degrees when starting
 void calibration() {
   // read initial distance
   distance = vl53.distance();
  
+  // arm is known to be in this range, keep rotating until this distance is found
   while (distance <= 55 || distance >= 80) {
+    // perform small steps, we don't know where the sensor is 
     myStepper.step(1);
     delay(5);
-    // Serial.println(vl53.dataReady());
- 
+
+    // remeasure distance
     if (vl53.dataReady()) {
       distance = vl53.distance();
-      // Serial.print(F("Distance: "));
-      // Serial.print(distance);
-      // Serial.println(" mm");
       vl53.clearInterrupt();
     }
   }
-  // this is a slop factor acounting for imperfect calibration
+ 
+  // the sensor will stop on the outer edge of the arm, add an extra 1.5 degrees to account
   myStepper.step(10);
+
+  // update flag so loop can run
   calibrated = true;
 }
+
